5eb5bfc6007cf19f7a75178cd73e2e55576fe6c5
Varun Santhanam <talkto@vsanthanam.com>
2021-05-19 17:17:36 -0700

    update docs

---
+++ gen-docs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+jazzy --clean --author Varun Santhanam --author_url https://www.vsanthanam.com --swift-build-tool spm --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 --module Ombi
