4dbac64a351ed843a5a5b7e9364acfb626f487c8
Varun Santhanam <talkto@vsanthanam.com>
2021-05-22 09:48:51 -0700

    Bump version to 1.0.1

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 jazzy \
     --clean \
-    --author Varun Santhanam \
+    --author "Varun Santhanam" \
     --author_url https://www.vsanthanam.com \
     --swift-build-tool spm \
     --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 \
     --module Ombi \
-    --module-version 1.0.0 \
-    --github_url https://ombi.network \
+    --module-version 1.0.1 \
+    --github_url https://www.github.com/vsanthanam/Ombi \
     --root-url https://docs.ombi.network \
     --output Documentation \
