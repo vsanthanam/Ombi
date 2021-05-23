c0e0eb7cea7bb251f44e59fbb652522150bac1d3
Varun Santhanam <talkto@vsanthanam.com>
2021-05-23 10:00:11 -0700

    Add complete test coverage to ComposableRequest

--- Tests/OmbiTests/ComposableRequestTests.swift
+++ Tests/OmbiTests/ComposableRequestTests.swift
@@ -32,12 +32,16 @@ class ComposableRequestTests: XCTestCase {
 
     func test_pathMethod_updatesPath() {
         var request = ComposableRequest<Any, Any, Error>()
+        
         XCTAssertEqual(request.path, "/")
+        
         request = request.path("/path")
+        
         XCTAssertEqual(request.path, "/path")
     }
 
     func test_pathClosure_updatesPath() {
+        
         final class PathProvider {
             var executed = false
             func provide() -> String {
@@ -45,24 +49,32 @@ class ComposableRequestTests: XCTestCase {
                 return "/path"
             }
         }
+        
         let provider = PathProvider()
         var request = ComposableRequest<Any, Any, Error>()
+        
         XCTAssertEqual(request.path, "/")
+        
         request = request.path { [provider] in
             provider.provide()
         }
+        
         XCTAssertEqual(request.path, "/path")
         XCTAssertTrue(provider.executed)
     }
 
     func test_methodMethod_updatesPath() {
         var request = ComposableRequest<Any, Any, Error>()
+        
         XCTAssertEqual(request.method, .get)
+        
         request = request.method(.post)
+        
         XCTAssertEqual(request.method, .post)
     }
 
     func test_methodClosure_updatesPath() {
+        
         final class MethodProvider {
             var executed = false
             func provide() -> RequestMethod {
@@ -70,14 +82,74 @@ class ComposableRequestTests: XCTestCase {
                 return .post
             }
         }
+        
         let provider = MethodProvider()
         var request = ComposableRequest<Any, Any, Error>()
+        
         XCTAssertEqual(request.method, .get)
+        
         request = request.method { [provider] in
             provider.provide()
         }
+        
         XCTAssertEqual(request.method, .post)
         XCTAssertTrue(provider.executed)
     }
 
+    func test_queryMethods_addQuery() {
+        var request = ComposableRequest<Any, Any, Error>()
+        
+        XCTAssertEqual(request.query, [])
+        
+        request = request.query(URLQueryItem(name: "1", value: "1"))
+        
+        XCTAssertEqual(request.query, [URLQueryItem(name: "1", value: "1")])
+        
+        request = request.query(URLQueryItem(name: "2", value: "2"))
+        
+        XCTAssertEqual(request.query, [URLQueryItem(name: "1", value: "1"),
+                                       URLQueryItem(name: "2", value: "2")])
+        
+        request = request.query(URLQueryItem(name: "3", value: "3"), URLQueryItem(name: "4", value: "4"))
+        
+        XCTAssertEqual(request.query, [URLQueryItem(name: "1", value: "1"),
+                                       URLQueryItem(name: "2", value: "2"),
+                                       URLQueryItem(name: "3", value: "3"),
+                                       URLQueryItem(name: "4", value: "4")])
+    }
+    
+    func test_queryClosures_addQuery() {
+        
+        final class QueryProvider {
+            var count = 0
+            func provide() -> URLQueryItem {
+                let item = URLQueryItem(name: (count+1).description, value: (count+1).description)
+                count += 1
+                return item
+            }
+        }
+        
+        
+        var request = ComposableRequest<Any, Any, Error>()
+        
+        let provider = QueryProvider()
+        
+        XCTAssertEqual(request.query, [])
+        
+        request = request.query { [provider] in
+            provider.provide()
+        }
+        
+        XCTAssertEqual(request.query, [URLQueryItem(name: "1", value: "1")])
+        XCTAssertEqual(provider.count, 1)
+
+        request = request.query { [provider] in
+            provider.provide()
+        }
+
+        XCTAssertEqual(request.query, [URLQueryItem(name: "2", value: "2"),
+                                       URLQueryItem(name: "3", value: "3")])
+        XCTAssertEqual(provider.count, 3)
+    }
+    
 }
